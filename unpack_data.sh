e95af0a0a95f6da6214e1bc2d0bb553c3bd59506
patelria007 <patelria07@gmail.com>
2021-09-23 23:02:40 -0400

    Added to unpack_data.sh to extract new tarball. Added script to reproduce plots.

--- unpack_data.sh
+++ unpack_data.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-mkdir -p data
-tar -xvzf EHTC_FirstM87Results_Apr2019_uvfits.tgz -C data --strip-components=1
+mkdir -p images
+mkdir -p data_validation/data
+tar -xvzf EHTC_FirstM87Results_Apr2019_uvfits.tgz -C data_validation/data --strip-components=1
+tar -xvzf EHTC_FirstM87Results_Apr2019_csv.tgz -C data_validation/data --strip-components=1
+mkdir -p data_validation/data/csv/converted
